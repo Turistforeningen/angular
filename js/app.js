911fc293cbe5ce98934d8d7752ed114469a6b38d
Hans Kristian Flaatten <hans@starefossen.com>
2013-04-10 08:55:21 +0200

    Adds more intricate example uses

--- js/app.js
+++ js/app.js
@@ -1,8 +1,124 @@
-angular.module('angular-rnd', []).
-    config(function($routeProvider) {
-        $routeProvider.
-            when('/about', {templateUrl:'partials/basic-template.html', controller:AboutCtrl}).
-            when('/experiments', {templateUrl:'partials/basic-template.html', controller:ExperimentsCtrl   }).
-            when('/', {templateUrl:'partials/basic-template.html', controller:HomeCtrl   }).
-            otherwise({redirectTo:'/'});
-    });
+function findById(a, id) {
+  for (var i=0; i<a.length; i++) {
+    if (a[i].id == id) return a[i];
+  }
+}
+
+angular.module('angular-rnd', ['ui.compat']).
+  config(
+    ['$stateProvider', '$routeProvider', '$urlRouterProvider',
+    function($stateProvider, $routeProvider, $urlRouterProvider) {
+      $urlRouterProvider.
+        /*when('/e?id', '/experiment/:id').*/
+        otherwise('/');
+
+      $routeProvider.
+        when('/', { templateUrl:'partials/basic-template.html', controller:HomeCtrl });
+
+      $stateProvider.
+        state('trips', {
+          url: '/trips',
+          abstract: true,
+          templateUrl: 'partials/trips/main.html',
+
+          /*resolve: {
+            trips: ['$timeout', '$stateParams', '$http', function($timeout, $stateParams, $http) {
+              $http.get('models/trips.json')
+                .then(function(res) {
+                  console.log('http', res);
+                  $timeout(function() {
+                    return res.data;
+                  }, 10);
+                  })
+            }]
+          },*/
+
+          controller:
+            ['$scope', '$state', '$http', function($scope, $state, $http) {
+              $scope.title = 'My Trips';
+              $scope.subTitle = 'Here are some of your trips';
+
+              $http.get('models/trips.json')
+                .then(function(res) {
+                  $scope.trips = res.data;
+                });
+
+              /* $scope.$on('$viewContentLoaded', function(){
+                console.log('state loaded');
+              }); */
+            }]
+        }).
+
+        state('trips.content', {
+          url: '',
+          views: {
+            'filter': {
+              templateUrl: 'partials/trips/filter.html',
+              controller: ['$scope', function($scope) {
+                console.log($scope);
+              }]
+            },
+            'list': {
+              templateUrl: 'partials/trips/list.html',
+              controller: ['$scope', function($scope) {
+                console.log($scope);
+              }]
+            }
+          }
+        }).
+
+        state('experiment', {
+          url: '/experiment',
+          abstract: true,
+          templateUrl: 'partials/experiment/main.html',
+          controller:
+            ['$scope', '$state', function($scope, $state) {
+              console.log($scope, $state);
+              $scope.title = 'Experiments';
+              $scope.items = [{
+                id: 0,
+                name: 'Item 0'
+              }, {
+                id: 1,
+                name: 'Item 1'
+              }];
+
+              // Some function
+              $scope.myNewFunction = function(x, y) {
+                return Math.pow(x, y)
+              };
+
+            }]
+        }).
+
+        state('experiment.empty', {
+          parent: 'experiment',
+          url: ''/*,
+          templateUrl: 'contacts.list.html'*/
+        }).
+
+        state('experiment.details', {
+          parent: 'experiment',
+          url: '/{id:[0-9]+}',
+          /*resolve: {
+            something:
+              ['$timeout', '$stateParams',
+              function ($timeout, $stateParams) {
+                return $timeout(function () { return "Asynchronously resolved data (" + $stateParams.id + ")" }, 10);
+              }]
+          },*/
+          views: {
+            'item': {
+              templateUrl: 'partials/experiment/details.html',
+              controller:
+                ['$scope', '$stateParams'/*, 'something'*/,
+                function($scope, $stateParams/*, something*/) {
+                  /*$scope.something = something;*/
+                  $scope.item = findById($scope.items, $stateParams.id);
+                  console.log($scope, $stateParams);
+                }]
+            }
+          }
+        })
+    }
+ ]);
--- js/controllers.js
+++ js/controllers.js
@@ -1,14 +1,22 @@
 function AboutCtrl($scope) {
-    $scope.title = 'About Page';
-    $scope.body = 'This is the about page body';
+  $scope.title = 'About Page';
+  $scope.body = 'This is the about page body';
 }
 
-function ExperimentsCtrl($scope) {
-    $scope.title = 'Experiments Page';
-    $scope.body = 'This is the about experiments body';
-}
+/*function ExperimentCtrl($scope, $location) {
+  $scope.title = 'Experiments Page';
+  $scope.body = 'This is the about experiments body';
+
+  $scope.$on('MyEvent', function( e, id ) {
+    console.log('MyEvent', id);
+    // $location.path('/experiments/' + id);
+    // $scope.count++;
+  });
+}*/
+
+// function
 
 function HomeCtrl($scope) {
-    $scope.title = 'Home Page';
-    $scope.body = 'This is the about home body';
+  $scope.title = 'Home Page';
+  $scope.body = 'This is the about home body';
 }
